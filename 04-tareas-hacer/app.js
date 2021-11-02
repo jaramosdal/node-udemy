e60ea73075676d6495287896395833edc5dd1bfe
Javier Ramos Nodal <javiramos@soliss.com>
2021-11-02 21:06:57 +0100

    Inquirer, Tarea, Tareas

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,22 +1,17 @@
 require('colors');
 
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
-
-console.clear();
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 
 const main = async () => {
-    console.log('Hola mundo')
-
     let opt = '';
 
     do {
-        opt = await mostrarMenu();
+        opt = await inquirerMenu();
         console.log(opt);
-        
-        if (opt !== '0') await pausa();
+        await pausa();
     } while (opt !== '0');
 
-    // pausa();
 }
 
 main();
---
+++ 04-tareas-hacer/models/tareas.js
@@ -0,0 +1,15 @@
+/**
+ * _listado:
+ *      { 'uuid-xxxxx-xxxxx-xxxxx': { id:xxxxx-xxxxx-xxxxx. desc:asd, completadoEn:20210810 } }
+ */
+
+
+class Tareas {
+    _listado = {};
+
+    constructor(){
+        this._listado = {};
+    }
+}
+
+module.exports = Tareas;
