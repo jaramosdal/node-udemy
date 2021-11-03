b11004df8820f38e5350ad19b87d2b0bf2ebe669
Javier Ramos Nodal <javiramos@soliss.com>
2021-11-03 19:34:47 +0100

    Guardado de tareas en archivo y carga inicial de las mismas

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,15 +1,41 @@
 require('colors');
 
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo')
+const { 
+    inquirerMenu, 
+    pausa, 
+    leerInput 
+} = require('./helpers/inquirer');
 const Tareas = require('./models/tareas');
 
 const main = async () => {
     let opt = '';
+    const tareas = new Tareas();
+    const tareasDB = leerDB();
+
+    if (tareasDB) {
+        // Establecer las tareas
+        tareas.cargarTareasFromArray(tareasDB);
+    }
 
     do {
+        // Imprimir el menú
         opt = await inquirerMenu();
-        console.log(opt);
-        await pausa();
+
+        switch (opt) {
+            case '1':
+                // crear opción
+                const desc = await leerInput('Descripción: ');
+                tareas.crearTarea(desc);
+                break;
+            case '2':
+                console.log(tareas.listadoArr)
+                break;
+        }
+
+        guardarDB(tareas.listadoArr);
+
+        if (opt !== '0') await pausa();
     } while (opt !== '0');
 
 }
--- 04-tareas-hacer/models/tareas.js
+++ 04-tareas-hacer/models/tareas.js
@@ -1,3 +1,5 @@
+const Tarea = require('./tarea');
+
 /**
  * _listado:
  *      { 'uuid-xxxxx-xxxxx-xxxxx': { id:xxxxx-xxxxx-xxxxx. desc:asd, completadoEn:20210810 } }
@@ -7,8 +9,29 @@
 class Tareas {
     _listado = {};
 
+    get listadoArr(){
+        const listado = [];
+        Object.keys(this._listado).forEach(key => {
+            const tarea = this._listado[key];
+            listado.push(tarea);
+        })
+
+        return listado
+    }
+
     constructor(){
         this._listado = {};
+    }
+
+    cargarTareasFromArray(tareas = []) {
+        tareas.forEach(tarea => {
+            this._listado[tarea.id] = tarea;
+        })
+    }
+
+    crearTarea(desc = ''){
+        const tarea = new Tarea(desc);
+        this._listado[tarea.id] = tarea;
     }
 }
 
