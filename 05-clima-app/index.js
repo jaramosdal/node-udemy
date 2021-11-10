de59a7b679a744daf540ceae14ed3cbd6e6130f1
Javier Ramos Nodal <javiramos@soliss.com>
2021-11-10 19:45:04 +0100

    Obtención de clima + historial

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -15,26 +15,39 @@ const main = async () => {
             case 1:
                 // Mostrar mensaje
                 const termino = await leerInput('Ciudad: ');
-                const lugares = await busquedas.ciudad(termino);
-                const id = await listadoLugares(lugares);
-                const lugarSeleccionado = lugares.find( l => l.id === id);
+                
                 // Buscar los lugares
-
+                const lugares = await busquedas.ciudad(termino);
+                
                 // Seleccionar el lugar
+                const id = await listadoLugares(lugares);
+                if (id === '0') continue;
 
+                const lugarSeleccionado = lugares.find( l => l.id === id);
+                
+                // Guardar en DB
+                busquedas.agregarHistorial(lugarSeleccionado.nombre)
+                
                 // Clima
+                const clima = await busquedas.climaLugar(lugarSeleccionado.lat, lugarSeleccionado.lng);
 
                 // Mostrar resultados
                 // console.clear();
                 console.log('\nInformación de la ciudad\n'.green)
-                console.log('Ciudad:', lugarSeleccionado.nombre);
+                console.log('Ciudad:', lugarSeleccionado.nombre.green);
                 console.log('Lat:', lugarSeleccionado.lat);
                 console.log('Lng:', lugarSeleccionado.lng);
-                console.log('Temperatura:',);
-                console.log('Mínima:',);
-                console.log('Máxima:',);
+                console.log('Temperatura:', clima.temp);
+                console.log('Mínima:', clima.min);
+                console.log('Máxima:', clima.max);
+                console.log('Cómo está el clima:', clima.desc.green);
                 break;
-        
+            case 2:
+                busquedas.historialCapitalizado.forEach((lugar, i) => {
+
+                    const idx = `${i+1}`.green;
+                    console.log(`${idx} ${lugar}`)
+                })
             default:
                 break;
         }
--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -1,10 +1,21 @@
+const fs = require('fs');
 const axios = require('axios');
 
 class Busquedas {
     constructor() {
         // leer DB si existe
 
-        this.historial = ['Tegucigalpa', 'Madrid', 'San José'];
+        this.historial = [];
+        this.dbPath = './db/database.json'
+        this.leerDB();
+    }
+
+    get historialCapitalizado(){
+        return this.historial.map(h => {
+            let palabras = h.split(', ');
+            palabras = palabras.map(word => word[0].toUpperCase() + word.substring(1));
+            return palabras.join(', ');
+        })
     }
 
     get paramsMapbox(){
@@ -12,6 +23,14 @@ class Busquedas {
             'access_token': process.env.MAPBOX_KEY,
             'limite': 5,
             'language': 'es'
+        };
+    }
+
+    get paramsWeather(){
+        return {
+            'appid': process.env.OPENWEATHER,
+            'units': 'metric',
+            'lang': 'es'
         };
     }
 
@@ -37,6 +56,60 @@ class Busquedas {
             return [];
         }
 
+    }
+
+    async climaLugar(lat, lon) {
+
+        try {
+            // petición http
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: { ...this.paramsWeather, lat, lon }
+            });
+
+            const resp = await instance.get();
+            const { weather, main } = resp.data;
+
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            };
+        } catch (error) {
+            return [];
+        }
+
+    }
+
+    agregarHistorial(lugar = ''){
+        // Prevenir duplicados
+        if (this.historial.includes(lugar.toLowerCase())) {
+            return;
+        }
+
+        this.historial.unshift(lugar.toLowerCase());
+        
+        this.historial = this.historial.splice(0, 5);
+
+        // Grabar en DB
+        this.guardarDB();
+    }
+
+    guardarDB(){
+        const payload = {
+            historial: this.historial
+        }
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload))
+    }
+
+    leerDB(){
+        if (!fs.existsSync(this.dbPath)) return;
+    
+        const info = fs.readFileSync(this.dbPath, { encoding: 'utf-8' });
+        const data = JSON.parse(info);
+    
+        this.historial = data.historial;
     }
 }
 
