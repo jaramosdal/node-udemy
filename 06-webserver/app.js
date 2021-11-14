9740f758f94eb1f2639b81a15b311abb6e24969e
Javier Ramos Nodal <javiramos@soliss.com>
2021-11-14 19:45:51 +0100

    Request y Response

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    res.write('Hola mundo');
+    // res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    // const persona = {
+    //     id: 1,
+    //     nombre: 'Fernando'
+    // };
+
+    // res.write(JSON.stringify(persona));
+
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    res.writeHead(200, { 'Content-Type': 'application/csv' });
+
+    res.write('id, nombreº\n');
+    res.write('1, Fernandoº\n');
+    res.write('2, Maríaº\n');
+    res.write('3, Juanº\n');
+    res.write('4, Pedroº\n');
     res.end();
 })
 .listen(8080);
