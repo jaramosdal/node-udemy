4f9760d1274731477691103a376b8d83d2351484
Javier Ramos Nodal <javiramos@soliss.com>
2021-11-18 21:20:22 +0100

    Vistas y vistas parciales

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const express = require('express')
-const app = express()
-const port = 8080;
+const hbs = require('hbs');
 
+const app = express()
+const port = process.env.PORT;
+
+// Handlebars
 app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials')
+
 
 // Servir contenido estático
 app.use(express.static('public'))
@@ -15,11 +21,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html')
+    res.render('generic', { 
+        nombre: 'Javier Ramos Nodal',
+        titulo: 'Curso de Node'
+    });
 })
 
 app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html')
+    res.render('elements', { 
+        nombre: 'Javier Ramos Nodal',
+        titulo: 'Curso de Node'
+    });
 })
 
 app.listen(port, () => {
