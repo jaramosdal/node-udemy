7a5d2f3d6cd9a3c2acac08259b95d5cd29872cf9
Javier Ramos Nodal <javiramosnodal@gmail.com>
2021-11-22 21:17:11 +0100

    Leer datos de entrada

--- 07-restserver/controllers/usuarios.js
+++ 07-restserver/controllers/usuarios.js
@@ -1,20 +1,37 @@
-const { response } = require('express');
+const { response, request } = require('express');
 
-const usuariosGet = (req, res = response) => {
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
     res.json({
-        msg: 'get API - controlador'
+        msg: 'get API - controlador',
+        q, 
+        nombre, 
+        apikey,
+        page,
+        limit
     });
 }
 
 const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+    const { nombre, edad } = req.body;
+
     res.status(400).json({
-        msg: 'put API - controlador'
+        msg: 'put API - controlador',
+        id
     });
 }
 
 const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
     res.json({
-        msg: 'post API - controlador'
+        msg: 'post API - controlador',
+        nombre, 
+        edad
     });
 }
 
