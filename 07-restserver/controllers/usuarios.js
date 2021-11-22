66d9c2a89e6f087f694c9bff2b684dd4bc79a3e8
Javier Ramos Nodal <javiramosnodal@gmail.com>
2021-11-22 20:43:41 +0100

    Rutas y controladores

---
+++ 07-restserver/controllers/usuarios.js
@@ -0,0 +1,39 @@
+const { response } = require('express');
+
+const usuariosGet = (req, res = response) => {
+    res.json({
+        msg: 'get API - controlador'
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+    res.status(400).json({
+        msg: 'put API - controlador'
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+    res.json({
+        msg: 'post API - controlador'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - controlador'
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
---
+++ 07-restserver/models/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('../routes/usuarios');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+    middlewares() {
+        // CORS
+        this.app.use(cors());
+
+        // Directorio Público
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port)
+        })
+    }
+}
+
+module.exports = Server;
