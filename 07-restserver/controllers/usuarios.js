9ea782e2ccee05076de2994558fbe157213552c7
Javier Ramos Nodal <javiramos@soliss.com>
2021-11-24 19:16:24 +0100

    Creación de usuarios en MongoDB

--- 07-restserver/controllers/usuarios.js
+++ 07-restserver/controllers/usuarios.js
@@ -1,4 +1,5 @@
 const { response, request } = require('express');
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -24,14 +25,15 @@ const usuariosPut = (req, res = response) => {
     });
 }
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async(req, res = response) => {
 
-    const { nombre, edad } = req.body;
+    const body = req.body;
+    const usuario = new Usuario(body);
+
+    await usuario.save();
 
     res.json({
-        msg: 'post API - controlador',
-        nombre, 
-        edad
+        usuario
     });
 }
 
