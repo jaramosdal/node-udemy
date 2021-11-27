9699a15cc03efb04a149f351d5218d20bacd495d
Javier Ramos Nodal <javiramos@soliss.com>
2021-11-27 11:38:18 +0100

    Valido rol contra BD

--- 07-restserver/routes/usuarios.js
+++ 07-restserver/routes/usuarios.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const Role = require('../models/role');
 
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios');
 const { validarCampos } = require('../middlewares/validar-campos');
@@ -13,7 +14,13 @@ router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe de ser más de 6 letras').isLength({ min:6 }),
     check('correo', 'El correo no es válido').isEmail(),
-    check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom( async(rol = '') => {
+        const existeRol = await Role.findeOne({rol});
+        if (!existeRol) {
+            throw new Error(`El rol ${rol} no está registrado en la BD`)
+        }
+    }),
     validarCampos
 ], usuariosPost);
 
